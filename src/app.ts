1bf1ee56547c332185107cecb87ce47096d36ad8
SeoJaeWan <sjw7324@naver.com>
2024-12-02 20:33:13 +0900

    feat: test api

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import { Socket, Server } from "socket.io";
 import http from "http";
+import express from "express";
 
+const app = express();
 const server = http.createServer();
 
 const port = 4000;
+
+app.get("/", (req, res) => {
+  console.log("Hello World");
+
+  res.send("Hello World");
+});
 
 const io = new Server(server);
 
