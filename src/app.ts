50a6a9032909da54e3354cca3e892925bc614687
SeoJaeWan <sjw7324@naver.com>
2024-12-02 20:40:29 +0900

    feat: test api

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import http from "http";
 import express from "express";
 
 const app = express();
-const server = http.createServer();
+const server = http.createServer(app);
 
 const port = 4000;
 
